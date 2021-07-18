52a538b7a633b027cdec2494be4d86264b433a32
Mobyrr <mobyrm11@gmail.com>
2021-07-18 22:46:08 +0200

    configuration dependant of servers, now work with messageUpdate, too many important bug fixes

--- src/MapFile.ts
+++ src/MapFile.ts
@@ -12,8 +12,8 @@ export default class MapFile {
         this.fillDataFromFile();
     }
 
-    private async fillDataFromFile() {
-        let data = (await fs.promises.readFile(this.path)).toString();
+    private fillDataFromFile() {
+        let data = fs.readFileSync(this.path, { flag: "a+" }).toString();
         let list = data.split("\n");
         for (let line of list) {
             if (line.trim() === "") continue;
@@ -25,9 +25,6 @@ export default class MapFile {
             }
             let d = line.split(MapFile.SPLITTER);
             this.data.set(d[0], d[1]);
-            
-            console.log(d[0] + " - " + d[1]);
-            
         }
     }
 
@@ -36,30 +33,29 @@ export default class MapFile {
     }
 
     public async set(key: String, value: String): Promise<void> {
-        if (key.includes(MapFile.SPLITTER) || value.includes(MapFile.SPLITTER)) throw "Key or value contain the splitter";
+        if (key.includes(MapFile.SPLITTER) || value.includes(MapFile.SPLITTER))
+            throw "Key or value contain the splitter";
         if (!this.data.has(key)) {
             await fs.promises.appendFile(this.path, "\n" + key + MapFile.SPLITTER + value);
         } else {
             let data = (await fs.promises.readFile(this.path)).toString();
-            let re = new RegExp('^' + key + MapFile.SPLITTER + '.*$', 'gm');
+            let re = new RegExp("^" + key + MapFile.SPLITTER + ".*$", "gm");
             let formatted = data.replace(re, key + MapFile.SPLITTER + value);
             await fs.promises.writeFile(this.path, formatted);
         }
 
         this.data.set(key, value);
-        
     }
 
     public async delete(key: String): Promise<boolean> {
         let result = this.data.delete(key);
         if (result) {
             let data = (await fs.promises.readFile(this.path)).toString();
-            let formatted = data.replace(new RegExp('^' + key + MapFile.SPLITTER + '.*', 'gm'), "");
-            formatted = formatted.replace(new RegExp('^(?:[\t ]*(?:\r?\n|\r))+', 'gm'), "");
+            let formatted = data.replace(new RegExp("^" + key + MapFile.SPLITTER + ".*", "gm"), "");
+            formatted = formatted.replace(new RegExp("^(?:[\t ]*(?:\r?\n|\r))+", "gm"), "");
             console.log(data);
             console.log(formatted);
             await fs.promises.writeFile(this.path, formatted);
-            
         }
         return result;
     }
--- src/index.ts
+++ src/index.ts
@@ -16,10 +16,13 @@ config({ path: resolve(__dirname, "../.env") });
 
 const client = new Client();
 const prefix = "!";
-let map = new MapFile("./wordBlackListMap");
+let maps = new Map<string, MapFile>();
 
 async function verifyMsg(msg: Message): Promise<void> {
-    let content = msg.content;
+    if (msg.guild === null) throw "Error";
+    let map = maps.get(msg.guild.id);
+    if (map === undefined) throw "Error";
+    let newContent = msg.content;
     let hasChanged = false;
     for (let key of map.keys()) {
         let done = false;
@@ -27,21 +30,30 @@ async function verifyMsg(msg: Message): Promise<void> {
             let start = -1;
             let j = 0;
             let letter = " ";
-            for (let i = 0; i < content.length; i++) {
-                if (content[i].toLowerCase() === key[j]) {
+            for (let i = 0; i < newContent.length; i++) {
+                if (newContent[i].toLowerCase() === key[j]) {
                     if (start == -1) start = i;
                     letter = key[j];
                     while (key[j] === letter || key[j] === " ") j++;
-                    while (content[i + 1]?.toLowerCase() === letter) i++;
+                    while (newContent[i + 1]?.toLowerCase() === letter) i++;
                     if (j == key.length) {
-                        content = content.slice(0, start) + map.get(key) + content.slice(i + 1, content.length);
-                        hasChanged = true;
+                        newContent =
+                            newContent.slice(0, start) +
+                            map.get(key) +
+                            newContent.slice(i + 1, newContent.length);
+                        if (!hasChanged) hasChanged = true;
                         break;
                     }
-                } else if (start > -1 && !" \n\t,?;.:/!§*µù%^¨$£ø=+}])°@\\_|-[({'#\"~&".includes(content[i])) {
+                } else if (
+                    start > -1 &&
+                    !" \n\t,?;.:/!§*µù%^¨$£ø=+}])°@\\_|-[({'#\"~&".includes(newContent[i])
+                ) {
                     start = -1;
+                    j = 0;
+                    i--; // restart reading at the same letter
+                    continue;
                 }
-                if (i == content.length - 1) {
+                if (i == newContent.length - 1) {
                     done = true;
                     break;
                 }
@@ -49,27 +61,39 @@ async function verifyMsg(msg: Message): Promise<void> {
         }
     }
     let botUser = msg.client.user;
-    if (botUser == undefined) {
-        console.log("wtf ??");
+    if (botUser == undefined) throw "Error";
+    if (
+        !(<TextChannel>msg.channel).members
+            .get(botUser.id)
+            ?.hasPermission(Permissions.FLAGS.MANAGE_WEBHOOKS)
+    ) {
+        msg.channel.send(
+            "Remplacement du message précédent échoué car il me manque la permission de gérer les webhooks."
+        );
         return;
     }
-    if (!(<TextChannel> msg.channel).members.get(botUser.id)?.hasPermission(Permissions.FLAGS.MANAGE_WEBHOOKS)) {
-        msg.channel.send("Remplacement du message précédent échoué car il me manque la permission de gérer les webhooks.");
+    if (
+        !(<TextChannel>msg.channel).members
+            .get(botUser.id)
+            ?.hasPermission(Permissions.FLAGS.MANAGE_MESSAGES)
+    ) {
+        msg.channel.send(
+            "Remplacement du message précédent échoué car il me manque la permission de gérer les messages."
+        );
         return;
     }
-    let wh : Webhook | undefined;
+    if (!hasChanged) return;
+    let wh: Webhook | undefined;
     let whError = "Échec de la création/récupération du webhook, veuillez insulter Mobyr !";
+    let nickname = msg.member?.nickname;
+    nickname = nickname == null ? msg.author.username : nickname;
+    let avatar = msg.author.avatarURL();
     try {
-        if (!hasChanged) return;
-        let nickname = msg.member?.nickname;
-        nickname = nickname == null ? msg.author.username : nickname;
-        let avatar = msg.author.avatarURL();
-        wh = (await (<TextChannel> msg.channel).fetchWebhooks()).find(w => w.name == nickname);
+        wh = (await (<TextChannel>msg.channel).fetchWebhooks()).find((w) => w.name == nickname);
         if (wh == undefined) {
-            wh = await (<TextChannel> msg.channel).createWebhook(
-                nickname, 
-                {avatar: <string>avatar}
-            );
+            wh = await (<TextChannel>msg.channel).createWebhook(nickname, {
+                avatar: <string>avatar,
+            });
         }
     } catch {
         msg.channel.send(whError);
@@ -79,8 +103,9 @@ async function verifyMsg(msg: Message): Promise<void> {
         msg.channel.send(whError);
         return;
     }
-    await wh.send(content);
+    await wh.send(newContent);
     wh.delete();
+    msg.delete();
 }
 
 function getContent(commands: string[], index: number) {
@@ -88,26 +113,32 @@ function getContent(commands: string[], index: number) {
 }
 
 client.on("ready", () => {
-    console.log(
-        `Ready as ${client.user?.tag} in ${client.guilds.cache.size} servers !`
-    );
+    console.log(`Ready as ${client.user?.tag} in ${client.guilds.cache.size} servers !`);
 });
 
 client.on("message", (msg) => {
     if (msg.author.bot) return;
-    if (msg.channel.type !== "text") return;
-    if (!msg.content.startsWith(prefix) || !msg.member?.hasPermission(Permissions.FLAGS.MANAGE_MESSAGES)) {
+    if (msg.channel.type !== "text" || msg.guild == undefined) return;
+    let map = maps.get(msg.guild.id);
+    if (map === undefined) {
+        map = new MapFile("maps/" + msg.guild.id);
+        maps.set(msg.guild.id, map);
+    }
+    if (
+        !msg.content.startsWith(prefix) ||
+        !msg.member?.hasPermission(Permissions.FLAGS.MANAGE_MESSAGES)
+    ) {
         verifyMsg(msg);
         return;
     }
-    let commands = msg.content
-        .slice(prefix.length)
-        .toLowerCase()
-        .trim()
-        .split(/\s+/);
+    let commands = msg.content.slice(prefix.length).trim().split(/\s+/);
     if (commands[0] === "add") {
         let usage =
-            "usage : `" + prefix + 'add "search value" "replace value"`\nMust not contain the character `' + MapFile.SPLITTER + "`.";
+            "usage : `" +
+            prefix +
+            'add "search value" "replace value"`\nMust not contain the character `' +
+            MapFile.SPLITTER +
+            "`.";
         if (commands.length < 2) {
             msg.channel.send(usage);
             return;
@@ -115,19 +146,21 @@ client.on("message", (msg) => {
         const regex = /^"\s*([^µ]+)\s*"\s*"\s*([^µ]+)\s*"\s*$/;
         let content = getContent(commands, 1);
         let r = regex.exec(content)?.values();
-        if (r == undefined) {
+        if (r === undefined) {
             msg.channel.send(usage);
             return;
         }
         r.next();
-        map.set(String(r?.next().value).trim(), String(r?.next().value).trim());
+        map.set(String(r.next().value).trim(), String(r.next().value).trim());
         msg.react("✅");
     } else if (commands[0] === "remove" && commands.length > 1) {
         if (commands.length < 1) return;
     } else if (commands[0] === "list") {
         let x = 1;
         let content = "";
+        let isEmpty = true;
         for (let key of map.keys()) {
+            if (isEmpty) isEmpty = false;
             x++;
             content += "`" + key + "`" + " => " + "`" + map.get(key) + "`\n";
             if (x >= 15) {
@@ -137,9 +170,24 @@ client.on("message", (msg) => {
         }
         if (x > 1) {
             msg.channel.send(content);
+        } else if (isEmpty) {
+            msg.channel.send("Aucune association enregistré.");
         }
     } else {
         verifyMsg(msg);
+    }
+});
+
+client.on("messageUpdate", async (oldMsg, msg) => {
+    if (msg.partial) msg = await msg.fetch();
+    if (msg.author.bot) return;
+    if (msg.channel.type !== "text" || msg.guild == undefined) return;
+    if (
+        !msg.content.startsWith(prefix) ||
+        !msg.member?.hasPermission(Permissions.FLAGS.MANAGE_MESSAGES)
+    ) {
+        verifyMsg(msg);
+        return;
     }
 });
 
